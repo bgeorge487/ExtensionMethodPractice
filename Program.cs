a7852ba10df0df06cbe5f4177c38985350f93d7c
Brian G <brigeo987@gmail.com>
2024-02-13 16:49:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static class LogAnalysis
+{
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    static string SubstringAfter(this string logLine) 
+    {        
+        return ""; 
+    }
+
+    // TODO: define the 'SubstringBetween()' extension method on the `string` type
+    static string SubstringBetween() { return ""; }
+
+    // TODO: define the 'Message()' extension method on the `string` type
+    static string Message() { return ""; }
+
+    // TODO: define the 'LogLevel()' extension method on the `string` type
+    static string LogLevel() { return ""; }
+}
